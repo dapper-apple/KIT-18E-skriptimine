9e3f64f190ae597ba5a33199210ed38aa1536d27
Toivo Pärnpuu <Toivo.Parnpuu@tptlive.ee>
2019-09-27 15:08:25 +0300

    täiendatud

--- Toivo.P/hyper-v-skript.ps1
+++ Toivo.P/hyper-v-skript.ps1
@@ -17,20 +17,28 @@ New-VM -Name "new 2" -MemoryStartupBytes 1GB -NewVHDPath $vhdasukoht\base.vhdx -
 #>
 
 
-function Kustuta-Masinad ()
+function Remove-MyVMs ()
 {
     Get-vm | Remove-VM 
-    Remove-Item C:\Hyper-V\vhd\*.vhdx
+    Remove-Item C:\Hyper-V\vhd*\*.vhdx
     Write-Host "Puhastatud"
 }
 
 
-
-
-
-function Loo-Masin ($Nimi, $VhdKaust, $VhdSuurus)
+function New-MyVM ($Nimi, $VhdKaust, $VhdSuurus, $cpucount)
 { 
     $vhd = $VhdKaust + $Nimi + ".vhdx"
-    New-VM -Name $Nimi -Generation 2 -NewVHDPath $vhd -NewVHDSizeBytes $VhdSuurus
+    $vm =  New-VM -Name $Nimi -Generation 2 -NewVHDPath $vhd -NewVHDSizeBytes $VhdSuurus
+    Set-VMProcessor -Count $cpucount -VMName $vm.VMName
+
+}
+
+
+$konf = Import-Csv C:\Users\toivo.parnpuu\Documents\GitHub\KIT-18E-skriptimine\Toivo.P\vm-conf.csv -UseCulture
+
+foreach ($e in $konf)
+{
+
+    New-MyVM -Nimi $e.Vmname -VhdKaust $e.vhd_dir -VhdSuurus 100GB -cpucount $e.CPU_count
 }
 
