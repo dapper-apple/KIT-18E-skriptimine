e45819b4cb576e6039d483755e3fa106868fa6a5
cuukzor <55737591+cuukzor@users.noreply.github.com>
2019-09-27 16:07:28 +0300

    Update hyper-v-skript.ps1
    ver 1.1

--- Tanel.S/hyper-v-skript.ps1
+++ Tanel.S/hyper-v-skript.ps1
@@ -1,24 +1,52 @@
+#
 <#
-Get-Command -module *hyper-v*
-Update-Help
-Get-Help New-VM
+  
+
+Get-Command -Module *hyper-v*
+update-help
+Get-help new-vm
+New-VM
+
+New-VM -Name "new 1" -MemoryStartupBytes 1GB
+mkdir c:\Hyper-V\vhd\
 
 
-mkdir "c:\hyperv\vhd\"
+New-VM -Name "new 2" -MemoryStartupBytes 1GB -NewVHDPath $vhdasukoht\base.vhdx -NewVHDSizeBytes 4GB -WhatIf
 
-New-VM -Name "New 1" -MemoryStartupBytes 1024MB -NewVHDPath $vhdasukoht\base.vhdx -NewVHDSizeBytes 4GB
+
 #>
 
-function Kustuta-Masinad()
+
+function Remove-MyVMs ()
 {
-    Get-VM | Remove-VM
-    Remove-Item C:\hyperv\vhd\*.vhdx
-    Write-Host "Kustutatud"
+    Get-vm | Remove-VM 
+    Remove-Item C:\Hyper-V\vhd*\*.vhdx
+    Write-Host "Puhastatud"
 }
 
-function Loo-Masin($Nimi, $VhdKaust, $VhdSuurus)
-{
-  
-    $Vhd = $VhdKaust + $Nimi + "HDD" + ".vhdx"
-    New-VM -Name $Nimi -Generation 2 -NewVHDPath $Vhd -NewVHDSizeBytes $VhdSuurus
+
+function New-MyVM ($Nimi, $VhdKaust, $VhdSuurus, $cpucount, $dynamic, $ram)
+{ 
+    $vhd = $VhdKaust + $Nimi + ".vhdx"
+    $vm =  New-VM -Name $Nimi -Generation 2 -NewVHDPath $vhd -NewVHDSizeBytes $VhdSuurus
+    Set-VMProcessor -Count $cpucount -VMName $vm.VMName
+    Set-VMMemory -DynamicMemoryEnabled $dynamic -VMMemory $ram
+   
 }
+
+
+$konf = Import-Csv C:\Users\opilane\Documents\GitHub\KIT-18E-skriptimine\Toivo.P\vm-conf.csv -Delimiter ";" -Encoding UTF8
+
+foreach ($e in $konf)
+{
+    $dynamic = $false
+    if ($e.dynamic_memory -icontains "TRUE")
+{
+    $dynamic = $true
+}
+    
+    New-MyVM -Nimi $e.Vmname -VhdKaust $e.vhd_dir -VhdSuurus 100GB -cpucount $e.CPU_count -dynamic $dynamic -ram ($e.'RAM[GB]' *1GB)
+
+}
+
+
